6254b3a6a245b01b2ebf86a2065c3626405f8b9d
simonmader17 <seticher.mader@gmail.com>
2022-11-02 23:33:11 +0100

    Work on starting screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Blur Fit',
-      theme: ThemeData(
-          fontFamily: ,
-          scaffoldBackgroundColor: Colors.grey[900],
-          textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
-                  foregroundColor: Colors.white,
-                  padding: const EdgeInsets.all(20)))),
-      home: const MyStartingScreen(),
-    );
+        title: 'Blur Fit',
+        theme: ThemeData(
+            fontFamily: "LilitaOne",
+            scaffoldBackgroundColor: const Color(0xff212121),
+            textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(18))),
+                    textStyle: const TextStyle(
+                        fontSize: 32,
+                        fontFamily: "LilitaOne",
+                        fontWeight: FontWeight.w400),
+                    padding: const EdgeInsets.all(20)))),
+        home: Scaffold(
+          body: SafeArea(
+              child: Stack(children: const [
+            MyStartingScreen(),
+            Positioned(
+                left: 17,
+                top: 8,
+                child: GradientText("Blur Fit",
+                    gradient: LinearGradient(
+                        begin: Alignment(-1, -1),
+                        end: Alignment(1, 1),
+                        colors: [Color(0xffff0000), Color(0xff3300c3)]),
+                    style: TextStyle(fontSize: 44, fontFamily: "JotiOne")))
+          ])),
+        ));
   }
 }
 
@@ -30,11 +48,35 @@ class MyStartingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
+    return Center(
+        child: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment(-1, -1),
+                    end: Alignment(1, 1),
+                    colors: [Color(0xffff0000), Color(0xff3300c3)]),
+                borderRadius: BorderRadius.all(Radius.circular(18))),
             child: TextButton(
-      onPressed: () {},
-      child: const Text("Import Image"),
-    )));
+              onPressed: () {},
+              child: const Text("Import Image"),
+            )));
+  }
+}
+
+class GradientText extends StatelessWidget {
+  const GradientText(this.text,
+      {super.key, required this.gradient, this.style});
+
+  final String text;
+  final TextStyle? style;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+        blendMode: BlendMode.srcIn,
+        shaderCallback: (bounds) => gradient
+            .createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+        child: Text(text, style: style));
   }
 }
